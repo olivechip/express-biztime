012638a0ba8c1881b9a06952f27bf5d38db488f3
Oliver <oliver@MSI>
2023-10-29 13:09:51 -0700

    initial setup

---
+++ app.js
@@ -0,0 +1,35 @@
+/** BizTime Express Application. */
+const express = require("express");
+const app = express();
+const ExpressError = require("./expressError");
+
+// Parse request bodies for JSON
+app.use(express.json());
+
+// Route Handlers
+const cRoutes = require("./routes/companies");
+app.use("/companies", cRoutes);
+
+const iRoutes = require("./routes/invoices");
+app.use("/invoices", iRoutes);
+
+/** 404 Handler */
+
+app.use(function(req, res, next) {
+  const err = new ExpressError("Not Found", 404);
+  return next(err);
+});
+
+/** General Error Handler */
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+
+  return res.json({
+    error: err,
+    message: err.message
+  });
+});
+
+
+module.exports = app;
---
+++ routes/companies.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const ExpressError = require("../expressError");
+const router = express.Router();
+const db = require("../db");
+
+router.get('/', async (req, res, next) => {
+    const results = await db.query(`SELECT * FROM companies`);
+    console.log(results);
+})
+
+module.exports = router;
